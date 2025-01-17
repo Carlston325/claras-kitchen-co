771e9107dbd8e41845d1fa4f3c28d4ae27b0cdde
Carlston325 <130921730+Carlston325@users.noreply.github.com>
2025-01-17 16:30:19 +0000

    pages re-routing (incomplete)

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Contact() {
   return (
     <main className="main-body">
       <div className="page-hero">
         <h1 className="page-title">Contact</h1>
+      </div>
+      <div>
+        <h2>Nothing Here Yet!</h2>
+        <Link to="/">Return Home</Link>
       </div>
     </main>
   );
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Menu() {
   return (
     <main className="main-body">
       <div className="page-hero">
         <h1 className="page-title">Menu</h1>
+      </div>
+      <div>
+        <h2>Nothing Here Yet!</h2>
+        <Link to="/">Return Home</Link>
       </div>
     </main>
   );
